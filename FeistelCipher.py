05be6904caee215ae73da3ed510ccbee2be1276d
MeloGuan <meloguan123@gmail.com>
2021-06-03 16:47:35 +1200

    updated Feistel

--- FeistelCipher.py
+++ FeistelCipher.py
@@ -1,17 +1,39 @@
 # https://cws.auckland.ac.nz/CWS/CourseWorkService.svc/cwt?cid=Feistel
 
-keys = [0x4331, 0xD04A]
-L0 = 0xD5B6
-R0 = 0x9DA8
+# Melo
+# keys = [0x4331, 0xD04A]
+# L0 = 0xD5B6
+# R0 = 0x9DA8
+# encryptionRound = 2
+# decryptionRound = 1
+
+keys = [0xE233, 0x1355]
+L0 = 0x243B
+R0 = 0x5050
 encryptionRound = 2
-decryptionRound = 1
+decryptionRound = 2
+
+# keys = [0xA227, 0x5E76]
+# L0 = 0xB879
+# R0 = 0x8B51
+# encryptionRound = 2
+# decryptionRound = 2
 
 def main():
+
     encryptionL0, encryptionR0 = encryption()
+    encryptionL0, encryptionR0 = get_correct_hex(encryptionL0), get_correct_hex(encryptionR0)
     print("encryption: {} {}".format(hex(encryptionL0)[2:].upper(), hex(encryptionR0)[2:].upper()))
-    decryptionL0, decryptionR0 = decryption(encryptionL0, encryptionR0)
+
+
+    decryptionL0, decryptionR0 = decryption((encryptionL0), (encryptionR0))
+    decryptionL0, decryptionR0 = get_correct_hex(decryptionL0), get_correct_hex(decryptionR0)
     print("decryptionRound: {} {}".format(hex(decryptionL0)[2:].upper(), hex(decryptionR0)[2:].upper()))
 
+def get_correct_hex(hex_num):
+    new_hex = "0x" + (str(hex(hex_num))[::-1][:4][::-1])
+    a_int = int(new_hex, 16)
+    return a_int
 
 def encryption():
     l0 = L0
